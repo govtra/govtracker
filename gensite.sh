af31f2e8361ca6cb8d1efdf18f1a55f2e7d33b5c
Govtracker <contact@govtracker.org>
2017-02-20 14:20:12 +0100

    Adding important section

--- gensite.sh
+++ gensite.sh
@@ -6,7 +6,19 @@ if [ ! -d "govtracker-data" ]; then
 fi
 
 mkdir work
-for j in govtracker-data/*/; do
+touch work/imp.html
+echo "Processing important"
+for i in govtracker-data/important/*.md; do
+    i2=`basename $i`
+    echo "# $i2"
+    echo "<p class='date'>${i2%.md}</p>`markdown $i`" > work/imp2.html
+    cat work/imp.html >> work/imp2.html
+    mv work/imp2.html work/imp.html
+done
+sed -e '/{{{CONTENT}}}/ {' -e "r work/imp.html" -e 'd' -e '}' templates/index_template.html > site/index.html
+echo "Done important\n"
+
+for j in govtracker-data/*_*/; do
     j2=`basename $j`
     j2=${j2%_entries}
     j2up=`echo "$j2" | tr '[:lower:]' '[:upper:]'`
@@ -37,7 +49,7 @@ for j in govtracker-data/*/; do
         mkdir site/$j2
     fi
     # HTML
-    sed -- 's/{{{COUNTRY}}}/'"$j2up"'/g' templates/index_template.html > work/index2.html
+    sed -- 's/{{{COUNTRY}}}/'"$j2up"'/g' templates/country_index_template.html > work/index2.html
     sed -- 's/{{{COUNTRY_S}}}/'"$j2"'/g' work/index2.html > work/index.html
     sed -e '/{{{CONTENT}}}/ {' -e "r work/content.html" -e 'd' -e '}' work/index.html > site/$j2/index.html
 
